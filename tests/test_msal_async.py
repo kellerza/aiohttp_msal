125ecff3b15f81efc67c394b5668fca6aa78f92c
Johann Kellerman <kellerza@gmail.com>
2025-03-04 05:38:06 +0200

    created int & typing

--- tests/test_msal_async.py
+++ tests/test_msal_async.py
@@ -1,6 +1,6 @@
 """Test the AsyncMSAL class."""
 
-from unittest.mock import patch
+# from unittest.mock import patch
 
 from aiohttp_msal.msal_async import AsyncMSAL, Session
 
@@ -15,20 +15,16 @@ def test_ses() -> None:
     assert str(ses.manager_name) == ""
 
 
-async def test_request() -> None:
-    session = Session(None, new=True, data={"session": {"mail": "j@k", "name": "j"}})
-    ses = AsyncMSAL(session)
-    with patch.object(ses, "async_get_token") as mock_token:
-        mock_token.return_value = {"access_token": ""}
+# async def test_request() -> None:
+#     session = Session(None, new=True, data={"session": {"mail": "j@k", "name": "j"}})
+#     ses = AsyncMSAL(session)
+#     with patch.object(ses, "async_get_token") as mock_token:
+#         mock_token.return_value = {"access_token": ""}
 
-        async with ses.request_ctx("get", "http://httpbin.org.get") as resp:
-            if resp.ok:
-                return
+#         async with ses.request_ctx("get", "http://httpbin.org.get") as resp:
+#             if resp.ok:
+#                 return
 
-        async with ses.get("http://httpbin.org.get") as resp:
-            if resp.ok:
-                return
-
-        # async with ses.get2("http://httpbin.org.get") as resp:
-        #     if resp.ok:
-        #         return
+#         async with ses.get("http://httpbin.org.get") as resp:
+#             if resp.ok:
+#                 return
